12c3488585e0bd78147a215536a156c9b41bb10a
Vadim Nosov <60423807+StrangerEagleAge@users.noreply.github.com>
2026-01-01 02:23:23 +0500

    Загрузил файлы 1.01.2026 в 2:22

---
+++ js/counting-numbers.js
@@ -0,0 +1,58 @@
+'use strict'
+
+window.addEventListener('load', windowLoad);
+
+function windowLoad() {
+	// Функция инициализации
+	function digitsCountersInit(digitsCountersItems) {
+		let digitsCounters = digitsCountersItems ? digitsCountersItems : document.querySelectorAll('[data-digits-counter]');
+		if (digitsCounters) {
+			digitsCounters.forEach(digitsCounter => {
+				digitsCountersAnimate(digitsCounter);
+			});
+		}
+	}
+	// Функция Анимации
+	function digitsCountersAnimate(digitsCounter) {
+		let startTimestamp = null;
+		const duration = parseInt(digitsCounter.dataset.digitsCounter) ? parseInt(digitsCounter.dataset.digitsCounter) : 1000;
+		const startValue = parseInt(digitsCounter.innerHTML);
+		const startPosition = 0;
+		const step = (timestamp) => {
+			if (!startTimestamp) startTimestamp = timestamp;
+			const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+			digitsCounter.innerHTML = Math.floor(progress * (startPosition + startValue));
+			if (progress < 1) {
+				window.requestAnimationFrame(step);
+			}
+		};
+		window.requestAnimationFrame(step);
+	}
+	// Запуск анимации
+	// digitsCountersInit();
+
+
+	// Запуск анимации при скролле
+	let options = { threshold: 0.3 }
+	let observer = new IntersectionObserver((entries, observer) => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				const targetElement = entry.target;
+				const digitsCountersItems = targetElement.querySelectorAll('[data-digits-counter]');
+				if (digitsCountersItems.length) {
+					digitsCountersInit(digitsCountersItems);
+				}
+
+				// с этой строкой анимация будет играть по 1му разу:
+				// observer.unobserve(targetElement);
+			}
+		});
+	}, options);
+
+	let sections = document.querySelectorAll('.counting_numbers');
+	if (sections.length) {
+		sections.forEach(section => {
+			observer.observe(section);
+		});
+	}
+}
